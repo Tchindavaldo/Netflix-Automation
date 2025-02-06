788a6254872b81882c401d4a43545cc9d5dea995
Debian <tchindavaldoblair@gmail.com>
2025-02-06 13:12:25 +0000

    modif en prod

--- playwrightService.js
+++ playwrightService.js
@@ -20,7 +20,7 @@ class PlaywrightService
               // this.browser = await chromium.launch( { headless: false } ); 
 
               this.browser = await chromium.launch( {
-                     // executablePath: '/usr/bin/google-chrome-stable',
+                        executablePath: '/usr/bin/google-chrome-stable',
                      // executablePath: '/usr/bin/chromium', // Vérifie si ce chemin fonctionne, sinon essaie '/usr/bin/google-chrome'
                      headless: true // Important pour Render
               } );;
